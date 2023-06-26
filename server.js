71694422cc0bc3abff59944f22982a3da7881e1f
yeah1997 <lee7778889@gmail.com>
2023-06-27 07:14:34 +0900

    add koa

---
+++ server.js
@@ -0,0 +1,25 @@
+// Koa
+const Koa = require("koa");
+const Router = require("koa-router");
+// Next JS
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "prod";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(router.routes());
+
+  server.listen(3000, () => {
+    console.log("Koa server is listening on 3000");
+  });
+});
