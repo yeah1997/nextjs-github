771df73335dd35fee75420c0a053e3fbbcc79eba
yeah1997 <lee7778889@gmail.com>
2023-07-01 11:00:50 +0900

    fix

--- store/store.js
+++ store/store.js
@@ -1,2 +1,32 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import ReduxThink from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const initialState = {
+  count: 0,
+};
+
+const ADD = "ADD";
+
+function counterReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD:
+      return { count: state.count + (action.num || 1) };
+    default:
+      return state;
+  }
+}
+
+const allReducers = combineReducers({
+  counter: counterReducer,
+});
+
+const store = createStore(
+  allReducers,
+  {
+    counter: initialState,
+  },
+  composeWithDevTools(applyMiddleware(ReduxThink))
+);
+
+export default store;
